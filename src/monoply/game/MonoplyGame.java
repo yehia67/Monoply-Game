760f9bf11644026772dd67c873d33de15512345e
yehia67 <yehiatarek67@gmail.com>
2017-10-01 09:30:25 +0200

    modify dice class

--- src/monoply/game/MonoplyGame.java
+++ src/monoply/game/MonoplyGame.java
@@ -15,7 +15,7 @@ public class MonoplyGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("yarab");
+       
     }
     
 }
