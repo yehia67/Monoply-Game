2a5a41e171739aa95e24a921b21c9f31995441be
Mohamed Mostafa <mohamedmostafa1505@gmail.com>
2017-10-16 02:45:02 +0200

    Some Visual updates and added house/hotel pngs

---
+++ src/monoply/game/Players.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monoply.game;
+
+/**
+ *
+ * @author yehia
+ */
+public class Players {
+    public int money = 1500 ,turns, place = 0; 
+     Dice dice;
+     public String name;
+    void move(){
+         turns += dice.getDice();
+    }
+    Players(String n){
+        name = n;
+    }
+    
+}
